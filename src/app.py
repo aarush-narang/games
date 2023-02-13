0b3f519c31cad3a9a95012fae4ba8a0ef611475b
aarush-narang <aarushnarang@gmail.com>
2023-02-12 22:47:00 -0800

    move all routes to app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,55 @@
-from flask import Flask
-from routes import main_router
-import os
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-app.register_blueprint(main_router.main_router, url_prefix='/')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/tictactoe')
+def ttt():
+    return render_template('ttt.html')
+
+@app.route('/rps')
+def rps():
+    return render_template('rps.html')
+
+@app.route('/hangman')
+def hangman():
+    return render_template('hangman.html')
+
+@app.get('/hangman/randomWord')
+def getHangmanWord():
+    difficulty = request.args.get('diff')
+
+    if difficulty == 'easy':
+        with open('./static/other/words/easyWords.txt') as f:
+            words = list(f)
+    elif difficulty == 'medium':
+        with open('./static/other/words/mediumWords.txt') as f:
+            words = list(f)
+    elif difficulty == 'hard':
+        with open('./static/other/words/hardWords.txt') as f:
+            words = list(f)
+    elif difficulty == 'impossible':
+        with open('./static/other/words/impossibleWords.txt') as f:
+            words = list(f)
+    else:
+        with open('./static/other/words/allwords.txt') as f:
+            words = list(f)
+    
+
+    word = random.choice(words).strip()
+    return word
+
+@app.route('/guess-user')
+def guessUser():
+    return render_template('guessUser.html')
+
+@app.route('/guess-comp')
+def guessComp():
+    return render_template('guessComp.html')
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('DOMAIN'), port=os.getenv('PORT'), ssl_context='adhoc', debug=True)
+    app.run(debug=True)
